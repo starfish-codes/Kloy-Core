b67365467957cefea337e819906e431c4d955ee0
DiedeClaessens <87484823+DiedeClaessens@users.noreply.github.com>
2021-08-17 10:30:31 +0200

    Added Content type and Accept structure

--- Sources/Core/Http.swift
+++ Sources/Core/Http.swift
@@ -13,6 +13,22 @@ enum Version {
     case Http1dot1, Http2
 }
 
+enum ContentType: String, CaseIterable{
+    case Json = "application/json"
+    case FromUrlEncoded = "application/x-www-form-urlencoded"
+    case Xml = "application/xml" 
+    case Pdf = "application/pdf"
+    case FormData = "multipart/form-data"
+    case Mixed = "multipart/mixed"
+    case OctedStream = "application/octet-stream"
+    case Csv = "text/csv",
+    case EventStream = "text/event-stream"
+    case Plain = "text/plain",
+    case Html = "text/html",
+    case Xml = "text/xml"
+    case Yaml = "text/yaml"
+}
+
 struct Status {
     let code: Int
     let description: String
@@ -37,6 +53,10 @@ struct Response {
     let headers: [Header]
     let version: Version
     let body: Body
+}
+
+struct Accept {
+    let contentType: [ContentType]
 }
 
 typealias Service = (Request) -> Response
