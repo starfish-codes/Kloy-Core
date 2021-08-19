73f0de3447a909330a48c112a138922767fcc352
DiedeClaessens <87484823+DiedeClaessens@users.noreply.github.com>
2021-08-19 20:08:25 +0200

    Added some filter composition idea

--- Kloyground.playground/Contents.swift
+++ Kloyground.playground/Contents.swift
@@ -79,3 +79,90 @@ print(response.status, response.body)
 
 
 
+//Idea for implementing the filters 
+
+precedencegroup ForwarApplication {
+    associativity: left
+}
+
+precedencegroup BackwarApplication {
+    associativity: right,
+    higherThan: ForwardApplication
+}
+
+precedencegroup ForwardComposition {
+  associativity: left
+  higherThan: ForwardApplication
+}
+
+precedencegroup BackwarComposition {
+  associativity: right
+  higherThan: BackwarApplication
+}
+
+infix operator |>: ForwarApplication
+
+func |> <A,B>(a:A, f:(A)->B)->B{
+    return f(a);
+}
+
+infix operator <|:BackwarApplication
+
+func <| <A,B>(f:(A) -> B, a: A){
+    return f(a);
+}
+
+infix operator >>>: ForwardComposition
+
+//The andThen from the paper
+func >>> <A,B,C>(f: @escaping (A) -> B, g: @escaping (B) -> c) -> ((A) -> C){
+    return { in a
+        g(f(a))
+    }
+}
+
+infix operator <<<: BackwardComposition
+
+func >>> <A,B,C>(f: @escaping (A) -> B, g: @escaping (B) -> c) -> ((A) -> C){
+    return { in a
+        f(g(a))
+    }
+}
+
+func logingFilter(_ servcie:Service) -> Service{
+    //do some cole loging stuff
+    print("I loged something")
+    return service
+}
+
+func unauthorizedService(req: Request) -> Response {
+    Response(status: .unauthorized, headers: [], version: req.version, body: .empty)
+}
+
+func checkAuth(_ service:Service) -> Service{
+    //Do some fancy auth checking 
+    print("I checked the auth header and dit some other fancy stuff")
+    //Something is wrong 
+    let wentWrong = true
+    if wentWrong{
+        return unauthorizedService
+    }
+    //auth checking was valid 
+    return servcie
+}
+//Could also do something like this or even combine the two 
+let authFilter: Filter = checkAuth; 
+
+//using forward -- I like this one better
+// first authFilter an logingFilter are combined into 1 function and the the teaServices is 'piped' into that function 
+let serviceWithfilters = teaService |> authFilter >>> logingFilter
+let runtime = Server(host: serviceWithfilters)
+let response = runtime.process(request: teaRequest)
+print(response.status, response.body)
+
+
+//using backward 
+let anotherServiceWithfilters = logingFilter <<< authFilter <| teaService 
+let runtime = Server(host: anotherServiceWithfilters)
+let response = runtime.process(request: teaRequest)
+print(response.status, response.body)
