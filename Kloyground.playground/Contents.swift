0a111a633557da745124b26f7b0cd46337e1a029
Markus Kuhnt <markus@starfish.team>
2021-08-20 10:57:24 +0200

    fixes to initial filter ideas

--- Kloyground.playground/Contents.swift
+++ Kloyground.playground/Contents.swift
@@ -78,3 +78,43 @@ let response = runtime.process(request: teaRequest)
 print(response.status, response.body)
 
 
+
+func logFilter(_ service: @escaping Service) -> Service {
+    //do some cool logging stuff
+    print("I loged something")
+    return service
+}
+
+
+func unauthorizedService(req: Request) -> Response {
+    Response(status: .unauthorized, headers: [], version: req.version, body: .empty)
+}
+
+func checkAuth(_ service: @escaping Service) -> Service {
+    //Do some fancy auth checking
+    print("I checked the auth header and dit some other fancy stuff")
+    //Something is wrong
+    let wentWrong = true
+    if wentWrong {
+        return unauthorizedService
+    }
+    // auth checking was valid
+    // return service
+}
+
+//Could also do something like this or even combine the two
+//let authFilter: Filter = checkAuth;
+
+//using forward -- I like this one better
+// first authFilter an logingFilter are combined into 1 function and the the teaServices is 'piped' into that function
+let serviceWithfilters = teaService |> checkAuth >>> logFilter
+let otherRuntime = Server(host: serviceWithfilters)
+let otherResponse = otherRuntime.process(request: teaRequest)
+print(otherResponse.status, otherResponse.body)
+
+//using backward
+
+let anotherServiceWithfilters = logFilter <<< checkAuth <| teaService
+let yetAnotherRuntime = Server(host: anotherServiceWithfilters)
+let yetAnotherResponse = yetAnotherRuntime.process(request: teaRequest)
+print(yetAnotherResponse.status, yetAnotherResponse.body)
---
+++ Sources/Core/Filter.swift
@@ -0,0 +1,49 @@
+//Idea for implementing the filters
+
+precedencegroup ForwardApplication {
+    associativity: left
+}
+//
+precedencegroup BackwardApplication {
+    associativity: right
+    higherThan: ForwardApplication
+}
+//
+precedencegroup ForwardComposition {
+  associativity: left
+  higherThan: ForwardApplication
+}
+
+precedencegroup BackwardComposition {
+  associativity: right
+  higherThan: BackwardApplication
+}
+
+infix operator |>: ForwardApplication
+
+public func |> <A,B>(a:A, f:(A)->B) -> B {
+    return f(a);
+}
+
+infix operator <|: BackwardApplication
+
+public func <| <A,B>(f:(A) -> B, a: A) -> B {
+    return f(a);
+}
+
+infix operator >>>: ForwardComposition
+
+//The andThen from the paper
+public func >>> <A,B,C>(f: @escaping (A) -> B, g: @escaping (B) -> C) -> ((A) -> C) {
+    return { a in
+        g(f(a))
+    }
+}
+
+infix operator <<<: BackwardComposition
+
+public func <<< <A,B,C>(f: @escaping (B) -> C, g: @escaping (A) -> B) -> ((A) -> C) {
+    return { a in
+        f(g(a))
+    }
+}
