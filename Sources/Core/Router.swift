c39bb8b213467224bd18c2f64990f76c48856368
Markus Kuhnt <markus@kuhnt.gmbh>
2021-08-29 08:19:38 +0200

    consider named parameter values in down-stream request

--- Sources/Core/Router.swift
+++ Sources/Core/Router.swift
@@ -1,10 +1,30 @@
 import Foundation
 
+struct NamedParamaterValue {
+    let name: String
+    let value: String
+}
+
+public struct SegmentMatch {
+    let parameterValue: NamedParamaterValue?
+    
+    init() { parameterValue = nil }
+    
+    init(name: String?, value: String?) {
+        if let name = name,
+           let value = value {
+            parameterValue = .init(name: name, value: value)
+        } else {
+            parameterValue = nil
+        }
+    }
+}
+
 public protocol Segment {
     var stringValue: String { get }
     var path: Path { get }
     
-    func match(_ string: String) -> Bool
+    func match(_ string: String) -> SegmentMatch?
 }
 
 public typealias Path = [Segment]
@@ -14,17 +34,17 @@ public struct Route {
     let method: HTTPMethod
 }
 
-public struct NamedParam: Segment {
-    public enum ParamType: String {
+public struct NamedParameter: Segment {
+    public enum ParameterType: String {
         case Int
         case String
         case UUID
     }
     
     let name: String
-    let type: ParamType
+    let type: ParameterType
 
-    public init(_ name: String, type: ParamType) {
+    public init(_ name: String, type: ParameterType) {
         self.name = name
         self.type = type
     }
@@ -32,14 +52,14 @@ public struct NamedParam: Segment {
     public var stringValue: String { "{\(name): \(type)}" }
     public var path: Path { [self] }
     
-    public func match(_ string: String) -> Bool {
+    public func match(_ string: String) -> SegmentMatch? {
         switch type {
         case .Int:
-            return Int(string) != nil
+            return Int(string) != nil ? SegmentMatch(name: name, value: string) : nil
         case .String:
-            return true
+            return SegmentMatch(name: name, value: string)
         case .UUID:
-            return Foundation.UUID(uuidString: string) != nil
+            return Foundation.UUID(uuidString: string) != nil ? SegmentMatch(name: name, value: string) : nil
         }
     }
 }
@@ -65,8 +85,8 @@ extension String: Segment {
     
     public var path: Path { self.split(separator: "/").map { String($0) } }
     
-    public func match(_ string: String) -> Bool {
-        self.lowercased() == string.lowercased()
+    public func match(_ string: String) -> SegmentMatch? {
+        self.lowercased() == string.lowercased() ? SegmentMatch() : nil
     }
 }
 
@@ -78,8 +98,8 @@ public typealias RoutedService = (Request) -> Response?
 
 func router(route: Route, service: @escaping Service) -> RoutedService {
     { request in
-        if match(route, with: request) {
-            return service(request)
+        if let routedRequest = match(route, with: request) {
+            return service(routedRequest)
         } else {
             return nil
             
@@ -87,21 +107,39 @@ func router(route: Route, service: @escaping Service) -> RoutedService {
     }
 }
 
-func match(_ route: Route, with request: Request) -> Bool {
-    return
-        route.method == request.method && match(route.path, with: request.uri)
+func match(_ route: Route, with request: Request) -> Request? {
+    guard route.method == request.method else { return nil }
+    var routedRequest = request
+    
+    if let pathMatch = match(route.path, with: request.uri) {
+        pathMatch.segmentMatches
+            .compactMap { $0.parameterValue }
+            .forEach { routedRequest.setNamedParameter(name: $0.name, value: $0.value) }
+        return routedRequest
+    } else {
+        return nil
+    }
 }
 
-func match(_ path: Path, with uri: String) -> Bool {
+struct PathMatch {
+    let segmentMatches: [SegmentMatch]
+}
+
+func match(_ path: Path, with uri: String) -> PathMatch? {
     let uriParts = uri.split(separator: "/")
     
     guard uriParts.count == path.count else {
-        return false
+        return nil
     }
     
-    return zip(path, uriParts)
-        .map { $0.match(String($1)) }
-        .reduce(true, { $0 && $1 })
+    let matchResult = zip(path, uriParts)
+        .compactMap { $0.match(String($1)) }
+    
+    guard matchResult.count == path.count else {
+        return nil
+    }
+    
+    return PathMatch(segmentMatches: matchResult)
 }
 
 infix operator ~>
--- Tests/CoreTests/RouterTests.swift
+++ Tests/CoreTests/RouterTests.swift
@@ -21,47 +21,47 @@ final class RouterTests: XCTestCase {
         let allCatsRoute = route(.Get, "api/v1/cats")
         let request = simpleRequest(uri: "/api/v1/cats")
         
-        XCTAssertTrue(match(allCatsRoute, with: request))
+        XCTAssertNotNil(match(allCatsRoute, with: request))
     }
     
     func testCaseInsenstiveMatch() {
         let allCatsRoute = route(.Get, "API/v1/caTs")
         let request = simpleRequest(uri: "/aPi/V1/CAts")
         
-        XCTAssertTrue(match(allCatsRoute, with: request))
+        XCTAssertNotNil(match(allCatsRoute, with: request))
     }
     
     func testNoMatch() {
-        let aCatsRoute  = route(.Get, "api/v1/cats", NamedParam("cat_id", type: .Int))
+        let aCatsRoute  = route(.Get, "api/v1/cats", NamedParameter("cat_id", type: .Int))
         let request = simpleRequest(uri: "/api/v1/cats")
         
-        XCTAssertFalse(match(aCatsRoute, with: request))
+        XCTAssertNil(match(aCatsRoute, with: request))
     }
     
     func testMatchStringParameter() {
-        let aCatsRoute  = route(.Get, "api", NamedParam("version", type: .String), "cats")
+        let aCatsRoute  = route(.Get, "api", NamedParameter("version", type: .String), "cats")
         let request = simpleRequest(uri: "/api/v1/cats")
         
-        XCTAssertTrue(match(aCatsRoute, with: request))
+        XCTAssertNotNil(match(aCatsRoute, with: request))
     }
     
     func testMatchIntParameter() {
-        let aCatsRoute  = route(.Get, "api", NamedParam("version", type: .Int), "cats")
+        let aCatsRoute  = route(.Get, "api", NamedParameter("version", type: .Int), "cats")
         
         let wrongRequest = simpleRequest(uri: "/api/v1/cats")
-        XCTAssertFalse(match(aCatsRoute, with: wrongRequest))
+        XCTAssertNil(match(aCatsRoute, with: wrongRequest))
         
         let goodRequest = simpleRequest(uri: "/api/1/cats")
-        XCTAssertTrue(match(aCatsRoute, with: goodRequest))
+        XCTAssertNotNil(match(aCatsRoute, with: goodRequest))
     }
     
     func testMatchUUIDParameter() {
-        let aCatRoute  = route(.Get, "api/v1", "cats", NamedParam("cat_id", type: .UUID))
+        let aCatRoute  = route(.Get, "api/v1", "cats", NamedParameter("cat_id", type: .UUID))
         
         let wrongRequest = simpleRequest(uri: "/api/v1/cats/test")
-        XCTAssertFalse(match(aCatRoute, with: wrongRequest))
+        XCTAssertNil(match(aCatRoute, with: wrongRequest))
         
         let goodRequest = simpleRequest(uri: "/api/v1/cats/114e0431-d939-485e-bf0c-ecfd566df419")
-        XCTAssertTrue(match(aCatRoute, with: goodRequest))
+        XCTAssertNotNil(match(aCatRoute, with: goodRequest))
     }
 }
