963985de20df9c55b721c2dde61fa1865b272e51
DiedeClaessens <87484823+DiedeClaessens@users.noreply.github.com>
2021-11-26 15:13:25 +0100

    Fixed Formatting

--- Sources/Core/Filter.swift
+++ Sources/Core/Filter.swift
@@ -1,40 +1,42 @@
-//Idea for implementing the filters
+// Idea for implementing the filters
 
 precedencegroup ForwardApplication {
     associativity: left
 }
+
 //
 precedencegroup BackwardApplication {
     associativity: right
     higherThan: ForwardApplication
 }
+
 //
 precedencegroup ForwardComposition {
-  associativity: left
-  higherThan: ForwardApplication
+    associativity: left
+    higherThan: ForwardApplication
 }
 
 precedencegroup BackwardComposition {
-  associativity: right
-  higherThan: BackwardApplication
+    associativity: right
+    higherThan: BackwardApplication
 }
 
 infix operator |>: ForwardApplication
 
-public func |> <A,B>(a:A, f:(A)->B) -> B {
-    return f(a);
+public func |> <A, B>(a: A, f: (A) -> B) -> B {
+    return f(a)
 }
 
 infix operator <|: BackwardApplication
 
-public func <| <A,B>(f:(A) -> B, a: A) -> B {
-    return f(a);
+public func <| <A, B>(f: (A) -> B, a: A) -> B {
+    return f(a)
 }
 
 infix operator >>>: ForwardComposition
 
-//The andThen from the paper
-public func >>> <A,B,C>(f: @escaping (A) -> B, g: @escaping (B) -> C) -> ((A) -> C) {
+// The andThen from the paper
+public func >>> <A, B, C>(f: @escaping (A) -> B, g: @escaping (B) -> C) -> ((A) -> C) {
     return { a in
         g(f(a))
     }
@@ -42,11 +44,8 @@ public func >>> <A,B,C>(f: @escaping (A) -> B, g: @escaping (B) -> C) -> ((A) ->
 
 infix operator <<<: BackwardComposition
 
-public func <<< <A,B,C>(f: @escaping (B) -> C, g: @escaping (A) -> B) -> ((A) -> C) {
+public func <<< <A, B, C>(f: @escaping (B) -> C, g: @escaping (A) -> B) -> ((A) -> C) {
     return { a in
         f(g(a))
     }
 }
-
-
-
--- Sources/Core/Http.swift
+++ Sources/Core/Http.swift
@@ -141,7 +141,6 @@ public struct Response {
     }
 }
 
-
 struct Accept {
     let contentType: [ContentType]
 
